3c2c6cf62376cde9c3f56e4bac1ea0448899165a
Bhaskar Ruidas <shonjay54@gmail.com>
2024-11-06 20:20:47 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.use(express.static("public"))   //pdf, images store
 
 app.use(cookieParser())
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter)      //https://localhost:8000/api/v1/users/register
 
 
 export { app }
